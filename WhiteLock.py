0a9496d08be453f91e8ce9fdc5dbbc25087bc9ab
sbm367 <sbm367@protonmail.com>
2019-04-17 23:25:32 -0400

    got open cv working but in cl envr so gui still issue

--- WhiteLock.py
+++ WhiteLock.py
@@ -1,3 +1,14 @@
-#import opencv
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-print("Init Commit")
+print('Init Commit')
+
+img =  cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+
+#cv2.imshow('Image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+plt.imshow(img, cmap='gray',interpolation='bicubic')
+plt.show()
